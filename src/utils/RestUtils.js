cef59597ca904e08a70f32d17257c1f2bf9bbe6a
lmurature <lucio.murature@mercadolibre.com>
2021-08-09 13:33:40 -0300

    development and production variables for heroku and local run configs

--- src/utils/RestUtils.js
+++ src/utils/RestUtils.js
@@ -1,26 +1,26 @@
-import store from 'store'
+import store from "store";
+
+const env = process.env.APP_ENV || "development";
 
 class RestUtils {
-
   static getApiUrl() {
-    return process.env.APP_ENV === "development"
-    ? "http://localhost:8080"
-    : "https://melist-api.herokuapp.com"; 
+    return env === "development"
+      ? "http://localhost:8080"
+      : "https://melist-api.herokuapp.com";
   }
 
-    static getHeaders() {
+  static getHeaders() {
     return {
       headers: {
-        Authorization: "Bearer " + store.get('access-token').access_token,
+        Authorization: "Bearer " + store.get("access-token").access_token,
       },
     };
   }
 
   static getAuthUrl() {
-    return process.env.APP_ENV === "development" ?
-  "https://auth.mercadolibre.com.ar/authorization?response_type=code&client_id=5112680121711673&redirect_uri=http://localhost:3000/auth/authorized" :
-  "https://auth.mercadolibre.com.ar/authorization?response_type=code&client_id=5112680121711673&redirect_uri=https://melist-app.herokuapp.com/auth/authorized";
-
+    return env === "development"
+      ? "https://auth.mercadolibre.com.ar/authorization?response_type=code&client_id=5112680121711673&redirect_uri=http://localhost:3000/auth/authorized"
+      : "https://auth.mercadolibre.com.ar/authorization?response_type=code&client_id=5112680121711673&redirect_uri=https://melist-app.herokuapp.com/auth/authorized";
   }
 }
 
