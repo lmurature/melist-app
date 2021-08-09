e37828f0756010feb135ad23e68e68a8fc266531
lmurature <lucio.murature@mercadolibre.com>
2021-08-08 21:37:09 -0300

    change the way the env works for heroku tests

--- src/utils/RestUtils.js
+++ src/utils/RestUtils.js
@@ -3,7 +3,7 @@ import store from 'store'
 class RestUtils {
 
   static getApiUrl() {
-    return process.env.NODE_ENV === "development"
+    return process.env.APP_ENV === "development"
     ? "http://localhost:8080"
     : "https://melist-api.herokuapp.com"; 
   }
@@ -17,7 +17,7 @@ class RestUtils {
   }
 
   static getAuthUrl() {
-    return process.env.NODE_ENV === "development" ?
+    return process.env.APP_ENV === "development" ?
   "https://auth.mercadolibre.com.ar/authorization?response_type=code&client_id=5112680121711673&redirect_uri=http://localhost:3000/auth/authorized" :
   "https://auth.mercadolibre.com.ar/authorization?response_type=code&client_id=5112680121711673&redirect_uri=https://melist-app.herokuapp.com/auth/authorized";
 
