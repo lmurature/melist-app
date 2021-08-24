7504570d3a98133df88b1b8f2557c8d4fd5ae2ba
lmurature <lucio.murature@mercadolibre.com>
2021-08-23 21:18:30 -0300

    minor style changes

--- src/components/ListCard.js
+++ src/components/ListCard.js
@@ -5,10 +5,25 @@ import React from "react";
 import PrivacyLabel from "./PrivacyLabel";
 
 const ListCard = (props) => {
+  let classes = [
+    "list-card-purple",
+    "list-card-red",
+    "list-card-green",
+    "list-card-brown",
+    "list-card-blue",
+    "list-card-pink",
+    "list-card-orange",
+  ];
+
+  const getRandomClass = () => {
+    let rng = parseInt(Math.random() * (classes.length - 1 - 0) + 0); // rand[0,8)
+    return `list-card unselectable ${classes[rng]}`;
+  };
+
   return (
     <React.Fragment>
       <Link to={`/lists/${props.id}`} className="link-to-list">
-        <div className="list-card unselectable" title={props.description}>
+        <div className={getRandomClass()} title={props.description}>
           <h1 className="list-title">{props.title}</h1>
         </div>
       </Link>
