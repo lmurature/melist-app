fcc788501ba0ba2e5027d413fa5ca3b796e6856f
lmurature <lucio.murature@mercadolibre.com>
2021-09-17 23:49:59 -0300

    loading state and managing error

--- src/pages/Summary.js
+++ src/pages/Summary.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Container, Button, Row, Col } from "react-bootstrap";
+import { Container, Button, Row, Col, Spinner, Alert } from "react-bootstrap";
 import ListCard from "../components/ListCard";
 import { PlusCircle } from "react-bootstrap-icons";
 import "./styles/Summary.scss";
@@ -14,6 +14,7 @@ const Summary = (props) => {
   const [sharedLists, setSharedLists] = useState([]);
   const [favouriteLists, setFavouriteLists] = useState([]);
   const [apiError, setApiError] = useState(null); // TODO: manage apiError.
+  const [loading, setLoading] = useState(true);
 
   const fetchData = async () => {
     try {
@@ -28,8 +29,11 @@ const Summary = (props) => {
 
       const shared = await ListsService.getSharedLists();
       setSharedLists(shared);
+
+      setLoading(false);
     } catch (err) {
       setApiError(err);
+      setLoading(false);
     }
   };
 
@@ -42,6 +46,9 @@ const Summary = (props) => {
 
   return (
     <Container className="main">
+      <Alert show={apiError} variant="danger" className="add-alert">
+        Hubo un error al cargar las listas.
+      </Alert>
       <h1 className="hi-title animate__animated animate__fadeIn">
         Hola {user && getCapitalizedName()}
         <span className="hi-emoji">👋</span>
@@ -54,80 +61,92 @@ const Summary = (props) => {
           </Link>
         </h2>
         <div className="my-lists">
-          <Row>
-            {lists.length ? (
-              lists.map((l, i) => {
-                return (
-                  <Col
-                    className="animate__animated animate__fadeIn"
-                    key={l.id}
-                    lg={2}
-                    md={3}
-                    xl={2}
-                    xs={6}
-                    xxl={2}
-                  >
-                    <ListCard
+          {loading ? (
+            <Spinner animation="border" role="status">
+              <span className="sr-only">Loading...</span>
+            </Spinner>
+          ) : (
+            <Row>
+              {lists.length ? (
+                lists.map((l, i) => {
+                  return (
+                    <Col
+                      className="animate__animated animate__fadeIn"
                       key={l.id}
-                      id={l.id}
-                      index={i}
-                      title={l.title}
-                      description={l.description}
-                      dateCreated={l.date_created}
-                      privacy={l.privacy}
-                      notifications={l.notifications}
-                    />
-                  </Col>
-                );
-              })
-            ) : (
-              <Container className="empty-state-text">
-                No tenés listas
-              </Container>
-            )}
-            <Col lg={2} md={3} xl={2} xs={6} xxl={2}>
-              <Link to="/list/create">
-                <Button className="cross-add-list-button">
-                  <PlusCircle />
-                </Button>
-              </Link>
-            </Col>
-          </Row>
+                      lg={2}
+                      md={3}
+                      xl={2}
+                      xs={6}
+                      xxl={2}
+                    >
+                      <ListCard
+                        key={l.id}
+                        id={l.id}
+                        index={i}
+                        title={l.title}
+                        description={l.description}
+                        dateCreated={l.date_created}
+                        privacy={l.privacy}
+                        notifications={l.notifications}
+                      />
+                    </Col>
+                  );
+                })
+              ) : (
+                <Container className="empty-state-text">
+                  No tenés listas
+                </Container>
+              )}
+              <Col lg={2} md={3} xl={2} xs={6} xxl={2}>
+                <Link to="/list/create">
+                  <Button className="cross-add-list-button">
+                    <PlusCircle />
+                  </Button>
+                </Link>
+              </Col>
+            </Row>
+          )}
         </div>
         <h2 className="shared-lists-heading">Compartidas conmigo 🙋‍♂️</h2>
         <div className="shared-lists">
-          <Row>
-            {sharedLists.length ? (
-              sharedLists.map((l, i) => {
-                return (
-                  <Col
-                    className="animate__animated animate__fadeIn"
-                    key={l.id}
-                    lg={2}
-                    md={3}
-                    xl={2}
-                    xs={6}
-                    xxl={2}
-                  >
-                    <ListCard
+          {loading ? (
+            <Spinner animation="border" role="status">
+              <span className="sr-only">Loading...</span>
+            </Spinner>
+          ) : (
+            <Row>
+              {sharedLists.length ? (
+                sharedLists.map((l, i) => {
+                  return (
+                    <Col
+                      className="animate__animated animate__fadeIn"
                       key={l.id}
-                      id={l.id}
-                      index={i}
-                      title={l.title}
-                      description={l.description}
-                      dateCreated={l.date_created}
-                      privacy={l.privacy}
-                      notifications={l.notifications}
-                    />
-                  </Col>
-                );
-              })
-            ) : (
-              <Container className="empty-state-text">
-                No tenés listas compartidas
-              </Container>
-            )}
-          </Row>
+                      lg={2}
+                      md={3}
+                      xl={2}
+                      xs={6}
+                      xxl={2}
+                    >
+                      <ListCard
+                        key={l.id}
+                        id={l.id}
+                        index={i}
+                        title={l.title}
+                        description={l.description}
+                        dateCreated={l.date_created}
+                        privacy={l.privacy}
+                        notifications={l.notifications}
+                      />
+                    </Col>
+                  );
+                })
+              ) : (
+                <Container className="empty-state-text">
+                  No tenés listas compartidas
+                </Container>
+              )}
+            </Row>
+          )}
         </div>
         <h2 className="favourites-heading">
           Favoritos 🌟{" "}
@@ -136,38 +155,44 @@ const Summary = (props) => {
           </Link>
         </h2>
         <div className="favourite-lists">
-          <Row>
-            {favouriteLists.length ? (
-              favouriteLists.map((l, i) => {
-                return (
-                  <Col
-                    className="animate__animated animate__fadeIn"
-                    key={l.id}
-                    lg={2}
-                    md={3}
-                    xl={2}
-                    xs={6}
-                    xxl={2}
-                  >
-                    <ListCard
+          {loading ? (
+            <Spinner animation="border" role="status">
+              <span className="sr-only">Loading...</span>
+            </Spinner>
+          ) : (
+            <Row>
+              {favouriteLists.length ? (
+                favouriteLists.map((l, i) => {
+                  return (
+                    <Col
+                      className="animate__animated animate__fadeIn"
                       key={l.id}
-                      id={l.id}
-                      index={i}
-                      title={l.title}
-                      description={l.description}
-                      dateCreated={l.date_created}
-                      privacy={l.privacy}
-                    />
-                  </Col>
-                );
-              })
-            ) : (
-              <Container className="empty-state-text">
-                No tenes ninguna lista favorita, podés explorar las listas
-                públicas
-              </Container>
-            )}
-          </Row>
+                      lg={2}
+                      md={3}
+                      xl={2}
+                      xs={6}
+                      xxl={2}
+                    >
+                      <ListCard
+                        key={l.id}
+                        id={l.id}
+                        index={i}
+                        title={l.title}
+                        description={l.description}
+                        dateCreated={l.date_created}
+                        privacy={l.privacy}
+                      />
+                    </Col>
+                  );
+                })
+              ) : (
+                <Container className="empty-state-text">
+                  No tenes ninguna lista favorita, podés explorar las listas
+                  públicas
+                </Container>
+              )}
+            </Row>
+          )}
         </div>
       </div>
     </Container>
