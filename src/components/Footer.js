574e6d5fa9387cdf7222c5e2504fdca1601897d1
lmurature <lucio.murature@mercadolibre.com>
2021-11-12 13:05:32 -0300

    added link in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,16 @@
-import "./styles/Footer.scss";
+import './styles/Footer.scss';
 
 const Footer = (props) => {
   return (
     <div className="footer">
       <p className="info">
-        Made with 💛 by Lucio Murature
+        Made with 💛 by{' '}
+        <a className="portfolio" href="https://lmurature-portfolio.netlify.app/" target="_blank">
+          Lucio Murature
+        </a>
       </p>
     </div>
   );
-}
+};
 
 export default Footer;
